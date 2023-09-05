a4c232f29d5a8017c35428f8d684752ab13c6f1a
agabani <agabani@users.noreply.github.com>
2023-09-06 00:17:08 +0100

    feat: add diagnostics

--- src/dev.rs
+++ src/dev.rs
@@ -1,10 +1,16 @@
-use bevy::prelude::*;
+use bevy::{diagnostic::*, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub(crate) struct DevPlugin;
 
 impl Plugin for DevPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(WorldInspectorPlugin::new());
+        app.add_plugins((
+            EntityCountDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+            LogDiagnosticsPlugin::default(),
+            SystemInformationDiagnosticsPlugin::default(),
+            WorldInspectorPlugin::new(),
+        ));
     }
 }

e5db2e5cb957b116d8dd69f32f17d969afb4dee8
agabani <agabani@users.noreply.github.com>
2023-09-29 01:22:35 +0100

    feat: main menu layout

--- src/scenes/main_menu/mod.rs
+++ src/scenes/main_menu/mod.rs
@@ -1,109 +1,301 @@
-mod camera;
-mod play_button;
-mod quit_button;
-mod settings_button;
+use bevy::{app::AppExit, prelude::*};
 
-use bevy::prelude::*;
+use crate::prelude::*;
 
-use crate::scenes::SceneState;
+use super::SceneState;
 
 #[allow(clippy::module_name_repetitions)]
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(SceneState::MainMenu), (camera::setup, setup))
-            .add_systems(OnExit(SceneState::MainMenu), (camera::teardown, teardown))
+        let state = SceneState::MainMenu;
+
+        app.add_systems(OnEnter(state), (spawn_camera, spawn_title))
+            .add_systems(OnExit(state), (despawn::<Camera>, despawn::<MainMenu>))
             .add_systems(
                 Update,
-                (
-                    play_button::interaction,
-                    quit_button::interaction,
-                    settings_button::interaction,
-                )
-                    .run_if(in_state(SceneState::MainMenu)),
+                (on_play_button_clicked, on_quit_button_clicked).run_if(in_state(state)),
             );
 
         #[cfg(feature = "dev")]
-        app.register_type::<MainMenu>()
-            .register_type::<play_button::PlayButton>()
-            .register_type::<quit_button::QuitButton>()
-            .register_type::<settings_button::SettingsButton>();
+        app.register_type::<Camera>()
+            .register_type::<CreditsButton>()
+            .register_type::<MainMenu>()
+            .register_type::<PlayButton>()
+            .register_type::<QuitButton>()
+            .register_type::<SettingsButton>();
     }
 }
 
 #[derive(Component)]
 #[cfg_attr(feature = "dev", derive(Reflect))]
-pub struct MainMenu;
+struct Camera;
+
+#[derive(Component)]
+#[cfg_attr(feature = "dev", derive(Reflect))]
+struct CreditsButton;
+
+#[derive(Component)]
+#[cfg_attr(feature = "dev", derive(Reflect))]
+struct MainMenu;
+
+#[derive(Component)]
+#[cfg_attr(feature = "dev", derive(Reflect))]
+struct PlayButton;
+
+#[derive(Component)]
+#[cfg_attr(feature = "dev", derive(Reflect))]
+struct QuitButton;
+
+#[derive(Component)]
+#[cfg_attr(feature = "dev", derive(Reflect))]
+struct SettingsButton;
 
 #[allow(clippy::needless_pass_by_value)]
-pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn despawn<T: Component>(mut commands: Commands, query: Query<Entity, With<T>>) {
+    for entity in &query {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((Name::new("Camera"), Camera, Camera2dBundle::default()));
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn spawn_title(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn((
+            Name::new("Main Menu"),
+            MainMenu,
             NodeBundle {
                 style: Style {
-                    // dimensions
-                    height: Val::Percent(100.0),
-                    width: Val::Percent(100.0),
-                    // flex
-                    align_items: AlignItems::Center,
-                    flex_direction: FlexDirection::Column,
-                    justify_content: JustifyContent::Center,
-                    row_gap: Val::Px(8.0),
-                    //
+                    height: Val::Vh(100.0),
+                    width: Val::Vw(100.0),
                     ..Default::default()
                 },
-                background_color: Color::rgb(0.15, 0.15, 0.15).into(),
                 ..Default::default()
             },
-            MainMenu,
-            Name::new("Main Menu"),
         ))
         .with_children(|parent| {
-            parent
-                .spawn((
-                    crate::components::button::ButtonBundle::new(),
-                    play_button::PlayButton,
-                    Name::new("Play Button"),
-                ))
-                .with_children(|parent| {
-                    parent.spawn(crate::components::text::TextBundle::new(
-                        &asset_server,
-                        "Play",
-                    ));
-                });
+            parent.spawn(NodeBundle {
+                style: Style {
+                    max_width: Val::Px(200.0),
+                    min_width: Val::Px(20.0),
+                    width: Val::Vw(6.25),
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
 
             parent
-                .spawn((
-                    crate::components::button::ButtonBundle::new(),
-                    settings_button::SettingsButton,
-                    Name::new("Settings Button"),
-                ))
+                .spawn(NodeBundle {
+                    background_color: Color::ORANGE.with_a(0.25).into(),
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        height: Val::Vh(100.0),
+                        max_width: Val::Px(460.0),
+                        min_width: Val::Px(260.0),
+                        width: Val::Vw(90.0),
+                        padding: UiRect {
+                            left: Val::Px(16.0),
+                            right: Val::Px(16.0),
+                            bottom: Val::Px(0.0),
+                            top: Val::Px(0.0),
+                        },
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
                 .with_children(|parent| {
-                    parent.spawn(crate::components::text::TextBundle::new(
-                        &asset_server,
-                        "Settings",
-                    ));
-                });
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            max_height: Val::Px(160.0),
+                            min_height: Val::Px(20.0),
+                            height: Val::Vh(100.0),
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    });
 
-            parent
-                .spawn((
-                    crate::components::button::ButtonBundle::new(),
-                    quit_button::QuitButton,
-                    Name::new("Quit Button"),
-                ))
-                .with_children(|parent| {
-                    parent.spawn(crate::components::text::TextBundle::new(
-                        &asset_server,
-                        "Quit",
+                    parent.spawn((
+                        Name::new("Bevy Breakout"),
+                        TextBundle {
+                            text: Text {
+                                alignment: TextAlignment::Left,
+                                sections: vec![TextSection {
+                                    style: TextStyle {
+                                        color: Color::rgb(1.0, 1.0, 1.0),
+                                        font: asset_server.load(ASSET_FONT_FIRA_SANS_BOLD.path()),
+                                        font_size: 60.0,
+                                    },
+                                    value: "BEVY BREAKOUT".to_string(),
+                                }],
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        },
                     ));
+
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            max_height: Val::Px(60.0),
+                            min_height: Val::Px(0.0),
+                            height: Val::Percent(100.0),
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    });
+
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::Column,
+                                row_gap: Val::Px(16.0),
+                                height: Val::Percent(100.0),
+                                width: Val::Percent(100.0),
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        })
+                        .with_children(|parent| {
+                            parent
+                                .spawn((
+                                    Name::new("Play"),
+                                    PlayButton,
+                                    ButtonBundle {
+                                        background_color: Color::WHITE.with_a(0.0).into(),
+                                        ..Default::default()
+                                    },
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn(TextBundle {
+                                        text: Text {
+                                            alignment: TextAlignment::Left,
+                                            sections: vec![TextSection {
+                                                style: TextStyle {
+                                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                                    font: asset_server
+                                                        .load(ASSET_FONT_FIRA_SANS_BOLD.path()),
+                                                    font_size: 36.0,
+                                                },
+                                                value: "PLAY".to_string(),
+                                            }],
+                                            ..Default::default()
+                                        },
+                                        ..Default::default()
+                                    });
+                                });
+
+                            parent
+                                .spawn((
+                                    Name::new("Settings"),
+                                    SettingsButton,
+                                    ButtonBundle {
+                                        background_color: Color::WHITE.with_a(0.0).into(),
+                                        ..Default::default()
+                                    },
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn(TextBundle {
+                                        text: Text {
+                                            alignment: TextAlignment::Left,
+                                            sections: vec![TextSection {
+                                                style: TextStyle {
+                                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                                    font: asset_server
+                                                        .load(ASSET_FONT_FIRA_SANS_BOLD.path()),
+                                                    font_size: 36.0,
+                                                },
+                                                value: "SETTINGS".to_string(),
+                                            }],
+                                            ..Default::default()
+                                        },
+                                        ..Default::default()
+                                    });
+                                });
+
+                            parent
+                                .spawn((
+                                    Name::new("Credits"),
+                                    CreditsButton,
+                                    ButtonBundle {
+                                        background_color: Color::WHITE.with_a(0.0).into(),
+                                        ..Default::default()
+                                    },
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn(TextBundle {
+                                        text: Text {
+                                            alignment: TextAlignment::Left,
+                                            sections: vec![TextSection {
+                                                style: TextStyle {
+                                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                                    font: asset_server
+                                                        .load(ASSET_FONT_FIRA_SANS_BOLD.path()),
+                                                    font_size: 36.0,
+                                                },
+                                                value: "CREDITS".to_string(),
+                                            }],
+                                            ..Default::default()
+                                        },
+                                        ..Default::default()
+                                    });
+                                });
+
+                            parent
+                                .spawn((
+                                    Name::new("Quit"),
+                                    QuitButton,
+                                    ButtonBundle {
+                                        background_color: Color::WHITE.with_a(0.0).into(),
+                                        ..Default::default()
+                                    },
+                                ))
+                                .with_children(|parent| {
+                                    parent.spawn(TextBundle {
+                                        text: Text {
+                                            alignment: TextAlignment::Left,
+                                            sections: vec![TextSection {
+                                                style: TextStyle {
+                                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                                    font: asset_server
+                                                        .load(ASSET_FONT_FIRA_SANS_BOLD.path()),
+                                                    font_size: 36.0,
+                                                },
+                                                value: "QUIT".to_string(),
+                                            }],
+                                            ..Default::default()
+                                        },
+                                        ..Default::default()
+                                    });
+                                });
+                        });
                 });
         });
 }
 
 #[allow(clippy::needless_pass_by_value)]
-pub fn teardown(mut commands: Commands, query: Query<Entity, With<MainMenu>>) {
-    for entity in &query {
-        commands.entity(entity).despawn_recursive();
+fn on_play_button_clicked(
+    mut next_state: ResMut<NextState<SceneState>>,
+    query: Query<&Interaction, (Changed<Interaction>, With<PlayButton>)>,
+) {
+    for &interaction in &query {
+        if interaction == Interaction::Pressed {
+            next_state.set(SceneState::Level);
+        }
+    }
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn on_quit_button_clicked(
+    mut event: EventWriter<AppExit>,
+    query: Query<&Interaction, (Changed<Interaction>, With<QuitButton>)>,
+) {
+    for &interaction in &query {
+        if interaction == Interaction::Pressed {
+            event.send(AppExit);
+        }
     }
 }
--- src/scenes/main_menu/settings_button.rs
+++
@@ -1,8 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Component)]
-#[cfg_attr(feature = "dev", derive(Reflect))]
-pub struct SettingsButton;
-
-#[allow(clippy::needless_pass_by_value)]
-pub fn interaction() {}
